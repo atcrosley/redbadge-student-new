bbe80203255c38bf8ea76e74eb5cb8b8bd87a843
Austin Crosley <atcrosley@gmail.com>
2021-03-27 16:18:32 -0400

    dice

---
+++ red-badge-demo-ts/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface FooterProps {
+    
+}
+ 
+const Footer: React.SFC<FooterProps> = () => {
+    return (<div>Let em' roll </div>);
+}
+ 
+export default Footer ;
